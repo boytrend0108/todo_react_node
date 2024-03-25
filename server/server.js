4aaba690f0aee399b8523c227146acc7e75e00f1
boytrend0108 <boytrend@gmail.com>
2024-03-25 16:40:15 +0200

    add: express routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
-import * as todosControllers from './controllers/todo.controller.js';
+import { router as todoRouter } from './routes/todo.route.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,15 +9,7 @@ const server = express();
 
 server.use(cors({ origin: 'http://localhost:5173' })); // add cors middleware
 
-server.get('/todos', todosControllers.get);
-
-server.get('/todos/:id', todosControllers.getOne);
-
-server.post('/todos', express.json(), todosControllers.create);
-
-server.delete('/todos/:id', todosControllers.remove);
-
-server.put('/todos/:id', express.json(), todosControllers.update);
+server.use('/todos', express.json(), todoRouter);
 
 server.listen(PORT, () => {
   console.log(`Server run on http:localhost:${PORT}`);
