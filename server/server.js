106d9eb183154f791dc7f8ee8372348c2a099add
boytrend0108 <boytrend@gmail.com>
2024-03-25 16:24:42 +0200

    add: controllers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
-import * as todosServises from './servises/todo.servise.js';
+import * as todosControllers from './controllers/todo.controller.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,60 +9,15 @@ const server = express();
 
 server.use(cors({ origin: 'http://localhost:5173' })); // add cors middleware
 
-server.get('/todos', (req, res) => {
-  res.send(todosServises.getAll());
-});
+server.get('/todos', todosControllers.get);
 
-server.get('/todos/:id', (req, res) => {
-  const { id } = req.params;
-  const todo = todosServises.getById(id);
+server.get('/todos/:id', todosControllers.getOne);
 
-  if (!todo) {
-    res.sendStatus(404);
+server.post('/todos', express.json(), todosControllers.create);
 
-    return;
-  }
+server.delete('/todos/:id', todosControllers.remove);
 
-  res.send(todo);
-});
-
-server.post('/todos', express.json(), (req, res) => {
-  const { todo } = req.body;
-  const newTodo = todosServises.create(todo);
-
-  if (!newTodo.todo) {
-    res.sendStatus(422);
-  }
-
-  res.statusCode = 201;
-  res.send(newTodo);
-});
-
-server.delete('/todos/:id', (req, res) => {
-  const { id } = req.params;
-
-  if (!todosServises.getById(id)) {
-    res.statusCode(404);
-    res.send('This todo not found');
-  }
-
-  todosServises.remove(id);
-  res.sendStatus(204);
-});
-
-server.put('/todos/:id', express.json(), (req, res) => {
-  const { id } = req.params;
-  const { todo, completed } = req.body;
-  const updatedTodo = todosServises.update({ id, todo, completed });
-
-  if (!updatedTodo) {
-    res.sendStatus(404);
-
-    return;
-  }
-
-  res.send(updatedTodo);
-});
+server.put('/todos/:id', express.json(), todosControllers.update);
 
 server.listen(PORT, () => {
   console.log(`Server run on http:localhost:${PORT}`);
